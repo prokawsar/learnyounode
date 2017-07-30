d9ab3fa06e29f9dad0b3f48fca98eacf20415c61
Kawsar Ahmed <prokawsar@users.noreply.github.com>
2017-07-31 00:23:44 +0600

    Update HTTP Client.js

--- HTTP Client.js
+++ HTTP Client.js
@@ -10,3 +10,13 @@ http.get(url, function(response) {
         console.log(error);
     })
 });
+
+/* Official Solution */
+
+var http = require('http')
+    
+    http.get(process.argv[2], function (response) {
+      response.setEncoding('utf8')
+      response.on('data', console.log)
+      response.on('error', console.error)
+    }).on('error', console.error)
