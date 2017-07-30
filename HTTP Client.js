7ded68c4f384e205b399ed9cc2d5a59579a045a0
Kawsar Ahmed <prokawsar@users.noreply.github.com>
2017-07-31 00:21:10 +0600

    Create HTTP Client.js

---
+++ HTTP Client.js
@@ -0,0 +1,12 @@
+var http = require("http");
+
+var url = process.argv[2];
+
+http.get(url, function(response) {
+    response.on('data', function(chunk) {
+        console.log(chunk.toString());
+    });
+    response.on('error', function(error) {
+        console.log(error);
+    })
+});
