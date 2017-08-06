daf6f9324fc6b7ca23e0fe35cea8fac43797c038
Kawsar Ahmed <prokawsar@users.noreply.github.com>
2017-08-06 13:07:31 +0600

    Update HTTP File Server.js

--- HTTP File Server.js
+++ HTTP File Server.js
@@ -1,3 +1,27 @@
 /*
 statement: https://hastebin.com/ivogaverab.pas
 */
+
+ var http = require('http')
+ var fs = require("fs")
+
+ var server = http.createServer(function(req, res) {
+     var file = fs.createReadStream(process.argv[3]);
+     console.log(file.pipe(res));
+
+ })
+ server.listen(process.argv[2]);
+ 
+ 
+/* Official Solution */
+
+var http = require('http')
+    var fs = require('fs')
+    
+    var server = http.createServer(function (req, res) {
+      res.writeHead(200, { 'content-type': 'text/plain' })
+    
+      fs.createReadStream(process.argv[3]).pipe(res)
+    })
+    
+    server.listen(Number(process.argv[2]))
